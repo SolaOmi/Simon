184d8cc6b3680b29ae4da49a15b0ece06caa87cf
sola <solaocodes@gmail.com>
2017-09-17 18:05:00 -0400

    Add game over function

---
+++ example.js
@@ -0,0 +1,3 @@
+for (var i = 0, j = 3; i < 3, j < 6; i++, j++ ) {
+    console.log(i, j);
+}
--- js/script.js
+++ js/script.js
@@ -6,25 +6,27 @@ var body = document.getElementsByTagName("body")[0];
 
 // Boxes
 var yellowBox = document.getElementById("yellow");
-var blueBox = document.getElementById("blue");
-var redBox = document.getElementById("red");
-var greenBox = document.getElementById("green");
+var blueBox   = document.getElementById("blue");
+var redBox    = document.getElementById("red");
+var greenBox  = document.getElementById("green");
 
 // Sounds
 var yellowSound = document.getElementById("yellowSound");
-var blueSound = document.getElementById("blueSound");
-var redSound = document.getElementById("redSound");
-var greenSound = document.getElementById("greenSound");
+var blueSound   = document.getElementById("blueSound");
+var redSound    = document.getElementById("redSound");
+var greenSound  = document.getElementById("greenSound");
 
 // Colors
-var yellow = "rgb(155,155,0)";
-var blue = "rgb(0,0,155)";
-var red = "rgb(155,0,0)";
-var green = "rgb(0,155,0)";
+var yellow       = "rgb(155,155,0)";
+var blue         = "rgb(0,0,155)";
+var red          = "rgb(155,0,0)";
+var green        = "rgb(0,155,0)";
 var brightYellow = "rgb(255,255,0)";
-var brightBlue = "rgb(0,0,255)";
-var brightRed = "rgb(255,0,0)";
-var brightGreen = "rgb(0,255,0)";
+var brightBlue   = "rgb(0,0,255)";
+var brightRed    = "rgb(255,0,0)";
+var brightGreen  = "rgb(0,255,0)";
+var white        = "rgb(255,255,255)";
+var black        = "rgb(0,0,0)";
 
 // Flash colors to Brigher version, play sound, then go back to original color
 function flashYellow() {
@@ -59,6 +61,12 @@ function flashGreen() {
     }, flashSpeed);
 }
 
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max-min)) + min;
+}
+
 function changeBackgroundColor() {
     var rValue = getRandomInt(0, 256);
     var gValue = getRandomInt(0, 256);
@@ -66,14 +74,17 @@ function changeBackgroundColor() {
     body.style.backgroundColor = "rgb(" + rValue + "," + gValue + "," + bValue + ")";
 }
 
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max-min)) + min;
+function gameOver() {
+    yellowSound.play();
+    blueSound.play();
+    redSound.play();
+    greenSound.play();
+    body.style.backgroundColor = "rgb(0,0,0)";
 }
 
-yellowBox.addEventListener("click", flashYellow);
-blueBox.addEventListener("click", flashBlue);
-redBox.addEventListener("click", flashRed);
-greenBox.addEventListener("click", flashGreen);
-body.addEventListener("click", changeBackgroundColor);
+// yellowBox.addEventListener("click", flashYellow);
+// blueBox.addEventListener("click", flashBlue);
+// redBox.addEventListener("click", flashRed);
+// greenBox.addEventListener("click", flashGreen);
+// body.addEventListener("click", changeBackgroundColor);
+// body.addEventListener("click", gameOver);
